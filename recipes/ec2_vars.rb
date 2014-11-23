6d7d0edad1f1e20903f788ee4b8be0e87e550f8c
Diego Lapiduz <diego.lapiduz@gsa.gov>
2014-11-22 23:19:36 -0800

    Strip spaces from s3 values

--- recipes/ec2_vars.rb
+++ recipes/ec2_vars.rb
@@ -2,6 +2,6 @@ if node[:ec2]
   prefix = node['app']['name'] + '/' + node['app']['environment']
   node.set['app']['ssl']['cert'] = citadel["#{prefix}/internal_cert"]
   node.set['app']['ssl']['key'] = citadel["#{prefix}/internal_cert_key"]
-  node.set['app']['s3']['access_key_id'] = citadel["#{prefix}/s3_access_key"]
-  node.set['app']['s3']['secret_access_key'] = citadel["#{prefix}/s3_secret"]
+  node.set['app']['s3']['access_key_id'] = citadel["#{prefix}/s3_access_key"].strip
+  node.set['app']['s3']['secret_access_key'] = citadel["#{prefix}/s3_secret"].strip
 end
