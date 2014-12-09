9f2531fdc345884db1e4bcb3476b10615d35a51f
Diego Lapiduz <diego.lapiduz@gsa.gov>
2014-12-09 11:09:20 -0800

    Pull sam api key from s3

--- recipes/ec2_vars.rb
+++ recipes/ec2_vars.rb
@@ -4,4 +4,5 @@ if node[:ec2]
   node.set['app']['ssl']['key'] = citadel["#{prefix}/internal_cert_key"]
   node.set['app']['s3']['access_key_id'] = citadel["#{prefix}/s3_access_key"].strip
   node.set['app']['s3']['secret_access_key'] = citadel["#{prefix}/s3_secret"].strip
+  node.set['app']['sam_api_key'] = citadel["#{prefix}/sam_api_key"].strip
 end
